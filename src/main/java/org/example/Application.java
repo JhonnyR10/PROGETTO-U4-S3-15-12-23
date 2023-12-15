ed084a2cc0a724e2fd18c88d73625d522fcd435f
Giovanni Longo <giovanni.longo10@icloud.com>
2023-12-15 15:05:26 +0100

    elementodao e test main

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,49 @@
 package org.example;
 
+import org.example.dao.ElementoDellaBibliotecaDAO;
+import org.example.dao.UtenteDAO;
+import org.example.entities.Libro;
+import org.example.entities.Periodicita;
+import org.example.entities.Rivista;
+import org.example.entities.Utente;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
 public class Application {
 
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("PROGETTO-U4-S3-15-12-23");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        ElementoDellaBibliotecaDAO ed = new ElementoDellaBibliotecaDAO(em);
+        UtenteDAO ud = new UtenteDAO(em);
+
+
+        Libro libro = new Libro("12nb", "Il Signore degli Anelli", 1954, 1200, "J.R.R. Tolkien", "Fantasy");
+        Rivista rivista = new Rivista("13bt", "National Geographic", 2023, 100, Periodicita.MENSILE);
+        Rivista rivista2 = new Rivista("13bl", "Novella 2000", 2023, 30, Periodicita.MENSILE);
+        Libro libro2 = new Libro("12nt", "Il Signore degli Anelli 2", 1960, 1200, "J.R.R. Tolkien", "Fantasy");
+        Utente utente1 = new Utente("Giovanni", "Longo", LocalDate.of(1992, 8, 10), 12);
+        Utente utente2 = new Utente("Francesco", "Longo", LocalDate.of(1993, 8, 10), 13);
+
+
+//        ed.save(libro);
+//        ed.save(rivista);
+//        ed.save(rivista2);
+//        ud.save(utente1);
+//        ud.save(utente2);
+
+
+        ed.getElementByISBN("12nb").forEach(System.out::println);
+        ed.searchByPublicationYear(1954).forEach(System.out::println);
+        ed.searchByAuthor("J.R.R. Tolkien").forEach(System.out::println);
+        ed.searchByTitle("signore").forEach(System.out::println);
+
+
+        em.close();
+        emf.close();
     }
 }
--- src/main/java/org/example/dao/ElementoDellaBibliotecaDAO.java
+++ src/main/java/org/example/dao/ElementoDellaBibliotecaDAO.java
@@ -1,4 +1,91 @@
 package org.example.dao;
 
+import org.example.entities.ElementoDellaBiblioteca;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class ElementoDellaBibliotecaDAO {
+    private final EntityManager em;
+
+    public ElementoDellaBibliotecaDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(ElementoDellaBiblioteca elemento) {
+        try {
+            EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(elemento);
+            transaction.commit();
+            System.out.println("Elemento " + elemento.getTitolo() + " salvato correttamente!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public ElementoDellaBiblioteca findById(long id) {
+        return em.find(ElementoDellaBiblioteca.class, id);
+    }
+
+    public void findByIdAndDelete(long id) {
+        try {
+            EntityTransaction t = em.getTransaction();
+            ElementoDellaBiblioteca found = em.find(ElementoDellaBiblioteca.class, id);
+            if (found != null) {
+                t.begin();
+                em.remove(found);
+                t.commit();
+                System.out.println("Elemento eliminato");
+            } else System.out.println("Elemento non trovato");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void removeElementoByISBN(String isbn) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Query deletebyISBNQuery = em.createQuery("DELETE FROM ElementoDellaBiblioteca e WHERE e.isbn = :isbn");
+        deletebyISBNQuery.setParameter("isbn", isbn);
+        int numeroDiElementiCancellati = deletebyISBNQuery.executeUpdate();
+        transaction.commit();
+
+        if (numeroDiElementiCancellati > 0) System.out.println("Elementi eliminati");
+        else System.out.println("Non ho trovato nessun elemento con ISBN " + isbn);
+    }
+
+    public List<ElementoDellaBiblioteca> getElementByISBN(String isbn) {
+        TypedQuery<ElementoDellaBiblioteca> query = em.createQuery(
+                "SELECT e FROM ElementoDellaBiblioteca e WHERE e.isbn = :isbn", ElementoDellaBiblioteca.class);
+        query.setParameter("isbn", isbn);
+
+        return query.getResultList();
+    }
+
+    public List<ElementoDellaBiblioteca> searchByPublicationYear(int year) {
+        TypedQuery<ElementoDellaBiblioteca> query = em.createQuery(
+                "SELECT e FROM ElementoDellaBiblioteca e WHERE e.annoPubblicazione = :year", ElementoDellaBiblioteca.class);
+        query.setParameter("year", year);
+        return query.getResultList();
+    }
+
+    public List<ElementoDellaBiblioteca> searchByAuthor(String author) {
+        TypedQuery<ElementoDellaBiblioteca> query = em.createQuery(
+                "SELECT e FROM ElementoDellaBiblioteca e WHERE e.autore = :author", ElementoDellaBiblioteca.class);
+        query.setParameter("author", author);
+        return query.getResultList();
+    }
+
+    public List<ElementoDellaBiblioteca> searchByTitle(String title) {
+        TypedQuery<ElementoDellaBiblioteca> query = em.createQuery(
+                "SELECT e FROM ElementoDellaBiblioteca e WHERE LOWER(e.titolo) LIKE LOWER(:title)", ElementoDellaBiblioteca.class);
+        query.setParameter("title", "%" + title + "%");
+        return query.getResultList();
+    }
 }
